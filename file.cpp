b0d0d6bb672129feb1b31e6d53595288097a4c28
Vytenis domasevicius <vytenis.domasevicius@vka.lt>
2020-10-08 18:15:20 +0300

    Added similarity comparisons, similar pairs function, now uses commandline arguments to switch between modes

--- file.cpp
+++ file.cpp
@@ -5,7 +5,41 @@
 #include<vector>
 #include <ctime>
 #include <chrono>
+#include <sstream>
 using namespace std;
+
+string hexCharToBinStr (char c)
+{
+    switch(c)
+    {
+        case '0': return "0000";
+        case '1': return "0001";
+        case '2': return "0010";
+        case '3': return "0011";
+        case '4': return "0100";
+        case '5': return "0101";
+        case '6': return "0110";
+        case '7': return "0111";
+        case '8': return "1000";
+        case '9': return "1001";
+        case 'a': return "1010";
+        case 'b': return "1011";
+        case 'c': return "1100";
+        case 'd': return "1101";
+        case 'e': return "1110";
+        case 'f': return "1111";
+    }
+    return "0000";
+}
+
+string getBinStrFromHexStr (string hex) {
+    string binStr = "";
+    for (int i = 0; i < hex.size(); i++) {
+        binStr += hexCharToBinStr(hex[i]);
+    }
+    return binStr;
+}
+
 long int checkPrimaryHash (long int hash) {
     // cout<<"Hash correction active"<<endl;
     const int MINIMUM = 300000000;
@@ -29,7 +63,14 @@ long int checkPrimaryHash (long int hash) {
 }
 long int lastHash = 1024;
 string lastInput = "";
+string lastFinalHex = "";
+
 double collisionCount = 0;
+double comparisonCount = 0;
+double binSumOfSimilarityPercentages = 0;
+double sumOfSimilarityPercentages = 0;
+double minSimilarityPercentage = 100;
+double maxSimilarityPercentage = 0;
 void createHash(string input, string secret) {
     long int hash = 1024;
     long int hashArr[8];
@@ -40,10 +81,9 @@ void createHash(string input, string secret) {
     }
     hash = checkPrimaryHash(hash);
     if (hash == lastHash) {
-        cout<<"COLLISION ["<<input<<","<<lastInput<<"]"<<endl;
+        // cout<<"COLLISION ["<<input<<","<<lastInput<<"]"<<endl;
         collisionCount++;
     }
-    // IivJa1yAZS,KgPfPABvaY
     lastHash = hash;
     lastInput = input;
     // cout<<"hash is : "<< hex << hash<<endl;
@@ -52,10 +92,45 @@ void createHash(string input, string secret) {
         hashArr[i-1] = checkPrimaryHash(hash * secret[pow(i, 4.75) / sqrt(i)]);
     }
 
+    string finalHex = "";
     for (int i = 0; i < 8; i++) {
         cout << hex << hashArr[i];
+
+        stringstream stream;
+        stream << hex << hashArr[i];
+        string result( stream.str() );
+        finalHex += result;
     }
     cout<<endl;
+
+    string finalBin = getBinStrFromHexStr(finalHex);
+    string lastFinalBin = getBinStrFromHexStr(lastFinalHex);
+    
+    double similarBin = 0;
+    for (int i = 0; i < finalBin.size(); i++) {
+        if (finalBin[i] == lastFinalBin[i]) {
+            similarBin++;
+        }
+    }
+    double binSimilarityPercentage = (similarBin / finalBin.size()) * 100;
+    binSumOfSimilarityPercentages += binSimilarityPercentage;
+
+    double similarHex = 0;
+    for (int i = 0; i < finalHex.size(); i++) {
+        if (finalHex[i] == lastFinalHex[i]) {
+            similarHex++;
+        }
+    }
+    double similarityPercentage = (similarHex / finalHex.size()) * 100;
+    sumOfSimilarityPercentages += similarityPercentage;
+    comparisonCount++;
+    if (similarityPercentage > maxSimilarityPercentage) {
+        maxSimilarityPercentage = similarityPercentage;
+    } else if (similarityPercentage < minSimilarityPercentage) {
+        minSimilarityPercentage = similarityPercentage;
+    }
+    // cout << "Similarity on hex level is - " << similarityPercentage << endl;
+    lastFinalHex = finalHex;
 }
 
 string readFromFile(string fileName) {
@@ -91,19 +166,21 @@ void readFromFileByLine (string fileName) {
     }
 }
 
-int main () {
-    int selection;
+int main (int argc, char *argv[]) {
+    int selection = atoi(argv[1]);
     string input;
     string givenFileName;
-    cout<<"Select Input Method"<<endl;
-    cout<<"1: File input"<<endl;
-    cout<<"2: Manual input"<<endl;
-    cout<<"3: Constitution"<<endl;
-    cout<<"4: Random Pairs"<<endl;
-    cin>>selection;
+    
     switch (selection){
+        case 0: {
+            cout<<"Select Input Method via CommandLine Argument"<<endl;
+            cout<<"1: File input"<<endl;
+            cout<<"2: Manual input"<<endl;
+            cout<<"3: Constitution"<<endl;
+            cout<<"4: Random Pairs"<<endl;
+            cout<<"5: Similar Pairs"<<endl;
+        }
         case 1: {
-            cin.ignore();
             cout<<"Enter file name"<<endl;
             getline(cin, givenFileName);
             input = readFromFile(givenFileName);
@@ -111,7 +188,6 @@ int main () {
             break;
         }
         case 2: {
-            cin.ignore();
             cout<<"Enter any string"<<endl;
             getline(cin, input);
             createHash(input, secret);
@@ -127,7 +203,23 @@ int main () {
         case 4: {
             auto start = std::chrono::steady_clock::now();
             readFromFileByLine("pairs.txt");
-            cout<<"Collision Percentage - "<<(collisionCount * 100) / 100000<<"%"<<endl;
+            cout << "Collision Percentage - " << (collisionCount * 100) / 100000 << "%" << endl;
+            cout << "Similarity Percentage Average - " << sumOfSimilarityPercentages / comparisonCount << "%" << endl;
+            cout << "Similarity Percentage Average (Binary-level) - " << binSumOfSimilarityPercentages / comparisonCount << "%" << endl;
+            cout << "Lowest Similarity Percentage - " << minSimilarityPercentage << "%" << endl;
+            cout << "Highest Similarity Percentage - " << maxSimilarityPercentage << "%" << endl;
+            auto ending = std::chrono::steady_clock::now();
+            cout<<"Done in : "<<std::chrono::duration <double, milli>(ending - start).count()<<" ms"<<endl;
+            break;
+        }
+        case 5: {
+            auto start = std::chrono::steady_clock::now();
+            readFromFileByLine("similarPairs.txt");
+            cout << "Collision Percentage - " << (collisionCount * 100) / 100000 << "%" << endl;
+            cout << "Similarity Percentage Average - " << sumOfSimilarityPercentages / comparisonCount << "%" << endl;
+            cout << "Similarity Percentage Average (Binary-level) - " << binSumOfSimilarityPercentages / comparisonCount << "%" << endl;
+            cout << "Lowest Similarity Percentage - " << minSimilarityPercentage << "%" << endl;
+            cout << "Highest Similarity Percentage - " << maxSimilarityPercentage << "%" << endl;
             auto ending = std::chrono::steady_clock::now();
             cout<<"Done in : "<<std::chrono::duration <double, milli>(ending - start).count()<<" ms"<<endl;
             break;
